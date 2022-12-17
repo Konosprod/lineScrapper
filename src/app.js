d3669e360e100344cead3562cba62e57db31e104
Konosprod <konosprod@free.fr>
2022-12-17 13:28:15 +0100

    Electron build working
    Signed-off-by: Konosprod <konosprod@free.fr>

---
+++ src/app.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require("electron");
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+
+  mainWindow.loadURL(`file://${__dirname}/../build/line-scrapper/index.html`);
+
+  //mainWindow.webContents.openDevTools();
+
+  mainWindow.on("closed", function () {
+    mainWindow = null;
+  });
+
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
+
+app.on("activate", function () {
+  if (mainWindow === null) createWindow();
+});
